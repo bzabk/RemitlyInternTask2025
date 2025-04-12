0a15e117498708dbbeeb4fd3dc9736b63be7891a
bzabk <bzabkowski@gmail.com>
2025-04-12 16:02:31 +0200

    init (folder structure)

---
+++ src/main/java/com/example/remitlyintern/RemitlyInternApplication.java
@@ -0,0 +1,13 @@
+package com.example.remitlyintern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemitlyInternApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RemitlyInternApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/remitlyintern/RemitlyInternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.remitlyintern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemitlyInternApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
